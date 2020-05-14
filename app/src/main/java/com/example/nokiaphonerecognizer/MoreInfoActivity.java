110950cfe115806f1bb687ebdb0cf23c0b511f3f
bastianhoj <bastianhoejbjerre@gmail.com>
2020-05-14 13:24:14 +0200

    Button click back to cameractivity from more-info activity

--- app/src/main/java/com/example/nokiaphonerecognizer/MoreInfoActivity.java
+++ app/src/main/java/com/example/nokiaphonerecognizer/MoreInfoActivity.java
@@ -2,14 +2,27 @@ package com.example.nokiaphonerecognizer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.annotation.Nullable;
 
 public class MoreInfoActivity extends Activity {
+    Button okButton;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_more_info);
+
+        okButton = (Button) findViewById(R.id.ok_button);
+
+        okButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
 
 
     }
