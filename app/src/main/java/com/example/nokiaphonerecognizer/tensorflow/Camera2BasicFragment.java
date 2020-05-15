26024efe5a6593eca86b28b2910417b571514c6a
bastianhoj <bastianhoejbjerre@gmail.com>
2020-05-15 15:27:39 +0200

    Cleaned up the code, deleted landscape xml file and updated manifest to only do portrait as orientation

--- app/src/main/java/com/example/nokiaphonerecognizer/tensorflow/Camera2BasicFragment.java
+++ app/src/main/java/com/example/nokiaphonerecognizer/tensorflow/Camera2BasicFragment.java
@@ -1,18 +1,3 @@
-/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
-
 package com.example.nokiaphonerecognizer.tensorflow;
 
 import android.app.Activity;
@@ -59,7 +44,6 @@ import androidx.core.content.ContextCompat;
 import androidx.legacy.app.FragmentCompat;
 
 import com.example.nokiaphonerecognizer.R;
-import com.example.nokiaphonerecognizer.tensorflow.AutoFitTextureView;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -334,8 +318,8 @@ public class Camera2BasicFragment extends Fragment
      */
     @Override
     public void onViewCreated(final View view, Bundle savedInstanceState) {
-        textureView = (AutoFitTextureView) view.findViewById(R.id.texture);
-        textView = (TextView) view.findViewById(R.id.text);
+        textureView = view.findViewById(R.id.texture);
+        textView = view.findViewById(R.id.text);
     }
 
     /**
