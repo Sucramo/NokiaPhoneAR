4d82c2854f5153282c4c16f4cfab0d12b062b9c7
marcu <mkol17@student.aau.dk>
2020-08-04 14:37:42 +0200

    No percentage numbers

--- app/src/main/java/com/example/nokiaphonerecognizer/objectDetection/tracking/MultiBoxTracker.java
+++ app/src/main/java/com/example/nokiaphonerecognizer/objectDetection/tracking/MultiBoxTracker.java
@@ -147,9 +147,10 @@ public class MultiBoxTracker {
 
             final String labelString =
                     !TextUtils.isEmpty(recognition.title)
-                            ? String.format("%s %.0f", "Nokia " + recognition.title, (100 * recognition.detectionConfidence))
-                            : String.format("%.2f", (""));
+                            ? String.format("%s", "Nokia " + recognition.title, (100 * recognition.detectionConfidence))
+                            : String.format("", (""));
                             // inclusion of the percentage sign for testing purposes
+                            //? String.format("%s %.0f", "Nokia " + recognition.title, (100 * recognition.detectionConfidence))
                             //: String.format("%.2f", (100 * recognition.detectionConfidence));
             //            borderedText.drawText(canvas, trackedPos.left + cornerSize, trackedPos.top,
             // labelString);
